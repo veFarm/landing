752d96054bf48dfd44a37056d099d6c81a30de75
fede-rodes <federodes@gmail.com>
2023-04-07 23:26:59 +0200

    copy and styles

--- src/App.svelte
+++ src/App.svelte
@@ -1,41 +1,32 @@
-<div class="px-4 py-8 mx-auto w-full md:px-24 lg:px-8 lg:py-16 bg-gray-800">
-  <div class="max-w-xl sm:mx-auto lg:max-w-2xl">
-    <div class="flex flex-col mb-8 sm:text-center sm:mb-0">
+<div class="bg-gray-800 w-full px-4 md:px-24 lg:px-8 py-8 lg:py-16">
+  <div
+    class="flex flex-col sm:items-center space-y-8 max-w-xl sm:mx-auto lg:max-w-3xl"
+  >
+    <!-- <div class="flex flex-col mb-8 sm:text-center sm:mb-0">
       <div
         class="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12"
-      >
-        <h2
-          class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-50 sm:text-4xl md:mx-auto"
-        >
-          The best way to trade your VTHO for VET.
-        </h2>
-        <p class="text-base text-gray-100 md:text-lg">
-          Our smart contract will spend your VTHO for VET, automatically. No
-          need to worry about the market conditions or finding the best time or
-          price!
-        </p>
-      </div>
-      <div>
-        <a
-          href="/"
-          class="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-indigo-500 hover:bg-indigo-500 focus:shadow-outline focus:outline-none"
-        >
-          Start now
-        </a>
-      </div>
-    </div>
+      > -->
+    <h1 class="sm:text-center">
+      Automate your <span class="inline-block">VTHO-VET</span> swaps.
+    </h1>
+    <p class="text-xl sm:text-2xl text-gray-100 sm:text-center">
+      Allow our smart contract to spend your VTHO in exchange for VET,
+      automatically. Don&apos;t worry about finding the best exchange rate or
+      the best time to trade!
+    </p>
+    <!-- </div>
+      <div> -->
+    <a
+      href="/"
+      class="inline-flex items-center justify-center text-xl sm:text-2xl h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-indigo-500 hover:bg-indigo-500 focus:shadow-outline focus:outline-none border-2 border-gray-100"
+    >
+      Start now
+    </a>
+    <!-- </div>
+    </div> -->
   </div>
 </div>
 <div class="px-4 py-8 mx-auto w-full bg-gray-50 md:px-24 lg:px-8 lg:py-16">
-  <!-- <div
-    class="max-w-xl mb-8 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12"
-  >
-    <h2
-      class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto text-center"
-    >
-      Stats
-    </h2>
-  </div> -->
   <div class="max-w-6xl mx-auto grid grid-cols-3 row-gap-8 md:grid-cols-3">
     <div class="text-center md:border-r border-gray-150">
       <h6 class="text-4xl font-bold lg:text-5xl xl:text-6xl text-gray-900">
