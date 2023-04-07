ee81093e28e86f671ada208c93bcbff36b383a6b
fede-rodes <federodes@gmail.com>
2023-04-07 18:01:01 +0200

    copy and stats

--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,71 @@
-<div class="flex flex-col justify-center space-y-8">
-  <h1>Automatically swap VTHO for VET to increase your VET balance.</h1>
-  <h2>
-    We use maths to determine the best trade based on your balance and market
-    conditions.
-  </h2>
-
-  <a href="#" class="inline-flex items-center justify-center">
-    Start
-    <svg
-      class="w-5 h-5 ml-2 mt-1"
-      fill="currentColor"
-      viewBox="0 0 20 20"
-      xmlns="http://www.w3.org/2000/svg"
-      ><path
-        fill-rule="evenodd"
-        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-        clip-rule="evenodd"
-      /></svg
-    >
-  </a>
+<div class="px-4 py-8 mx-auto w-full md:px-24 lg:px-8 lg:py-16 bg-gray-800">
+  <div class="max-w-xl sm:mx-auto lg:max-w-2xl">
+    <div class="flex flex-col mb-8 sm:text-center sm:mb-0">
+      <div
+        class="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12"
+      >
+        <h2
+          class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-50 sm:text-4xl md:mx-auto"
+        >
+          The best way to trade your VTHO for VET.
+        </h2>
+        <p class="text-base text-gray-100 md:text-lg">
+          Our smart contract will spend your VTHO for VET, automatically. No
+          need to worry about the market conditions or finding the best time or
+          price!
+        </p>
+      </div>
+      <div>
+        <a
+          href="/"
+          class="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-indigo-500 hover:bg-indigo-500 focus:shadow-outline focus:outline-none"
+        >
+          Start now
+        </a>
+      </div>
+    </div>
+  </div>
 </div>
-
-<style>
-  a {
-    background: linear-gradient(263deg, #5656b8 0%, hsl(318, 57%, 65%) 100%);
-  }
-</style>
+<div class="px-4 py-8 mx-auto w-full bg-gray-50 md:px-24 lg:px-8 lg:py-16">
+  <!-- <div
+    class="max-w-xl mb-8 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12"
+  >
+    <h2
+      class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto text-center"
+    >
+      Stats
+    </h2>
+  </div> -->
+  <div class="max-w-6xl mx-auto grid grid-cols-3 row-gap-8 md:grid-cols-3">
+    <div class="text-center md:border-r border-gray-150">
+      <h6 class="text-4xl font-bold lg:text-5xl xl:text-6xl text-gray-900">
+        3,2K
+      </h6>
+      <p
+        class="text-sm font-medium tracking-widest text-gray-900 uppercase lg:text-base"
+      >
+        Trades.
+      </p>
+    </div>
+    <div class="text-center md:border-r border-gray-150">
+      <h6 class="text-4xl font-bold lg:text-5xl xl:text-6xl text-gray-900">
+        13,8K
+      </h6>
+      <p
+        class="text-sm font-medium tracking-widest text-gray-900 uppercase lg:text-base"
+      >
+        VET volume.
+      </p>
+    </div>
+    <div class="text-center">
+      <h6 class="text-4xl font-bold lg:text-5xl xl:text-6xl text-gray-900">
+        486
+      </h6>
+      <p
+        class="text-sm font-medium tracking-widest text-gray-900 uppercase lg:text-base"
+      >
+        Users.
+      </p>
+    </div>
+  </div>
+</div>
